a629e03f7c93b3b55a06975312f26569a2e3551b
= <vanshjain.india@gmail.com>
2023-06-26 16:18:07 +0530

    bug fix(blank input)

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
+    if(inputValue){
+        push(shoppingListInDB, inputValue)
+    }
     
     clearInputFieldEl()
 })
